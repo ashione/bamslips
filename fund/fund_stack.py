186a866b460a1ee3401000aef3489b4155f2948c
ashione <skyzlxuan@gmail.com>
2017-05-30 00:36:24 +0800

    add get fund stack parall

--- fund/fund_stack.py
+++ fund/fund_stack.py
@@ -1,10 +1,16 @@
 import requests
 from bamslips.fund.fund_base import Fund_base
+from bamslips.conf.settings import DATA_ROOT
+from bamslips.conf.fund_code_list import code_list
 import json
 import glog
+import os
+from joblib import Parallel, delayed
 #coding=utf-8
 
+
 class Fund_stack(Fund_base) : 
+
     url_format = 'https://fundmobapi.eastmoney.com/FundMApi/FundInverstPositionNew.ashx?FCODE={code}&deviceid=Wap&plat=Wap&product=EFund&version=2.0.0'
 
     def get_fund_stack(self,code='000001'):
@@ -13,10 +19,18 @@ class Fund_stack(Fund_base) :
         resp_json = resp.json()
         return resp_json
 
+def get_fund_stack_info(code):
+    item = Fund_stack()
+    info = item.get_fund_stack(code)
+    json_file = '{:06d}.json'.format(code) if isinstance(code,int) else code+'.json'
+    json_file = os.path.join(DATA_ROOT,json_file)
+    with open(json_file,'w') as fp:
+        json.dump(info,fp,indent=4) 
+        glog.info('write {} fund stack into {}'.format(code,json_file))
+
+def paralle_get_stack_info_according_fund():
+    Parallel(n_jobs=4)(delayed(get_fund_stack_info)(code) for code in code_list)
 
 if __name__ == '__main__' :
-    item = Fund_stack()
-    info = item.get_fund_stack(code=3)
-    #glog.info(info['Datas']['fundboods'])
-    glog.info(info['Datas']['fundStocks'][0]['GPJC'])
-    
+    paralle_get_stack_info_according_fund()
+
